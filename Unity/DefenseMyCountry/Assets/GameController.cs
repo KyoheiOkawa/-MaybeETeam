2868972b25f8d7cb1a0da600b8f0d3d3764953b2
yu_honda <yhonda@mikilab.doshisha.ac.jp>
2017-09-16 16:53:00 +0900

    得点画面の作成

---
+++ Unity/DefenseMyCountry/Assets/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public const int CONST_GAME_OVER_COUNT = 3;
+	private bool isGameOver = false;
+	private int score = 0;
+	private int failureCount = 0;
+	public Text scoreText;
+
+	void start () {
+	}
+
+	void Update () {
+		if (this.isGameOver)
+		{			
+			PlayerPrefs.SetInt("Score", this.score);
+			PlayerPrefs.Save();
+			SceneManager.LoadScene ("resultScene");
+		}
+		scoreText.text = "Score : " + this.score.ToString("D4") + "\nFailed : " + this.failureCount;
+	}
+
+	void addScore () {
+		this.score += 10;
+	}
+
+	void addFailure () {
+		this.failureCount += 1;
+		if (this.failureCount == 3) {
+			this.isGameOver = true;
+		}
+	}
+
+}
+
---
+++ Unity/DefenseMyCountry/Assets/Scripts/Honda/scoreTextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreTextController : MonoBehaviour {
+
+	public Text scoreText;
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt("Score", 0);
+		PlayerPrefs.Save();
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		score = PlayerPrefs.GetInt("Score", score);
+		scoreText.text = "得点 :" + score;
+	}
+}
