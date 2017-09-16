78cacc7d09d559c573af6c800bbf629d7a7575f6
KyoheiOkawa <kurokenken0310@gmail.com>
2017-09-16 14:32:10 +0900

    とりあえず飛ぶ

---
+++ Unity/DefenseMyCountry/Assets/Scripts/Okawa/Missile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+	[SerializeField]
+	Vector3 moveDir = Vector3.up;
+
+	[SerializeField]
+	float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate (moveDir * moveSpeed * Time.deltaTime);	
+	}
+}
